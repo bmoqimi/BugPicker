ba5b707b84b7277ab8a480ae9bbfca72a6d6e47f
Babak Moghimi <babak.moqimi@gmail.com>
2014-09-08 18:37:39 +0200

    cat loading added

--- OPAL/demo/src/main/scala/org/opalj/bugpicker/bugPicker.scala
+++ OPAL/demo/src/main/scala/org/opalj/bugpicker/bugPicker.scala
@@ -27,21 +27,26 @@ import org.opalj.ai.debug.XHTML
 import org.opalj.br.analyses.BasicReport
 import org.opalj.util.PerformanceEvaluation._
 import scalafx.concurrent.Task
-import javafx.{concurrent => jfxc}
+import javafx.{ concurrent ⇒ jfxc }
 import scala.xml.Node
 import scalafx.event.EventType
 import scalafx.event.EventHandler
 import scalafx.concurrent.WorkerStateEvent
 import java.util.EventListener
-
+import scalafx.concurrent.WorkerStateEvent
+import javafx.collections.ObservableList
+import java.io.File
+import java.net.URL
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.control.ListView
 
 object bugPicker extends JFXApp {
     stage = new PrimaryStage {
         title = "Bug Picker"
-                width = 1900
-                height = 1200
-                scene = drawScene()
-                def drawScene() = {
+        width = 1900
+        height = 1200
+        scene = drawScene()
+        def drawScene() = {
             val thisScene = new Scene(1900, 1200) {
                 root = new BorderPane {
                     top = new VBox {
@@ -52,97 +57,110 @@ object bugPicker extends JFXApp {
             thisScene
         }
     }
-    var cp: String = null
-            var sourceDir: java.io.File = null
+    var loadedFiles: List[List[File]] = null
+    var sourceDir: java.io.File = null
+    var project: Project[URL] = null
 
-            def createViews() = {
+    def createViews() = {
 
-            val infoText = new TextArea {
-                text = "Please use the toolbar to load the project"
+        val infoText = new TextArea {
+            text = "Please use the toolbar to load the project"
+        }
+        val resultWebview = new WebView()
+        val sourceWebview = new WebView()
+        var files: List[java.io.File] = List()
+        var doc: Node = null
+        var br: BasicReport = null
+        object Worker extends Task(new jfxc.Task[String] {
+
+            protected def call(): String = {
+                //val project = new Project(files(0))
+                val deadCodeAnalysis = new DeadCodeAnalysis()
+                val results @ (analysisTime, methodsWithDeadCode) = deadCodeAnalysis.analyze(project, Seq.empty)
+                doc = XHTML.createXHTML(Some(deadCodeAnalysis.title), DeadCodeAnalysis.resultsAsXHTML(results))
+                br = BasicReport(
+                    methodsWithDeadCode.toList.sortWith((l, r) ⇒
+                        l.classFile.thisType < r.classFile.thisType ||
+                            (l.classFile.thisType == r.classFile.thisType && (
+                                l.method < r.method || (
+                                    l.method == r.method &&
+                                    l.ctiPC < r.ctiPC
+                                )
+                            ))
+                    ).mkString(
+                        "Dead code (number of dead branches: "+methodsWithDeadCode.size+"): \n",
+                        "\n",
+                        f"%nIdentified in: ${ns2sec(analysisTime)}%2.2f seconds."))
+                br.message
+
             }
-            val resultWebview = new WebView()
-            val sourceWebview = new WebView()
-            var files: List[java.io.File] = List()
-            var doc : Node = null
-            object Worker extends Task(new jfxc.Task[Any] {
 
-                protected def call() = {
-                    val project = Project(files(0))
-                            val deadCodeAnalysis = new DeadCodeAnalysis()
-                    val results @ (analysisTime, methodsWithDeadCode) = deadCodeAnalysis.analyze(project, Seq.empty)
-                    doc = XHTML.createXHTML(Some(deadCodeAnalysis.title), DeadCodeAnalysis.resultsAsXHTML(results))
-                    val br = BasicReport(
-                            methodsWithDeadCode.toList.sortWith((l, r) ⇒
-                                    l.classFile.thisType < r.classFile.thisType ||
-                                    (l.classFile.thisType == r.classFile.thisType && (
-                                            l.method < r.method || (
-                                                    l.method == r.method &&
-                                                    l.ctiPC < r.ctiPC
-                                                    )
-                                            ))
-                                    ).mkString(
-                                            "Dead code (number of dead branches: "+methodsWithDeadCode.size+"): \n",
-                                            "\n",
-                                            f"%nIdentified in: ${ns2sec(analysisTime)}%2.2f seconds."))
-                                            println(br.message)
+        })
 
-                }
+        def runAnalysis(files: List[java.io.File]) {
+            /**
+             * TODO: Prepare some pre-execute hook thing including a LOADING gif
+             */
+            /*
+            val outps = new ByteArrayOutputStream
+            val pst = new PrintStream(outps)
+            System.setOut(pst)
+            Console.setOut(pst)
+            */
+            val et = WorkerStateEvent.ANY
+            Worker.handleEvent(et) {
+                event: WorkerStateEvent ⇒
+                    {
+                        event.eventType match {
+                            case WorkerStateEvent.WORKER_STATE_SUCCEEDED ⇒ {
+                                resultWebview.engine.loadContent(doc.toString)
+                                infoText.text = br.message
+                            }
+                            case WorkerStateEvent.WORKER_STATE_SCHEDULED ⇒ {
+                                val loadingURL = getClass.getResource("/cat_loading.gif").toURI().toURL()
+                                resultWebview.engine.load(loadingURL.toString())
+                            }
+                        }
 
-
-            })
-
-
-            def runAnalysis(files: List[java.io.File]) {
-                /**
-                 * TODO: Prepare some pre-execute hook thing including a LOADING gif
-                 */
-
-                val outps = new ByteArrayOutputStream
-                        val pst = new PrintStream(outps)
-                System.setOut(pst)
-                Console.setOut(pst)
-
-                val thread = new Thread(Worker)
-                thread.setDaemon(true)
-                thread.start()
-                val et = WorkerStateEvent.WORKER_STATE_SUCCEEDED
-                Worker.handleEvent(et) {
-                    et => {
-                        resultWebview.engine.loadContent(doc.toString)
-                        outps.flush()
-                        pst.flush()
-                        infoText.text = outps.toString()
                     }
-                }
-                //val el = new EventListener()
 
-            //resultWebview.engine.loadContent(doc.toString)
-
+            }
+            val thread = new Thread(Worker)
+            thread.setDaemon(true)
+            thread.start()
 
         }
         val analyseButton = new Menu("Analysis") {
             items = List(
-                    new MenuItem("run") {
-                        //disable = true
-                        onAction = {
-                                e: ActionEvent ⇒ runAnalysis(files)
-                        }
+                new MenuItem("run") {
+                    //disable = true
+                    onAction = {
+                        e: ActionEvent ⇒ runAnalysis(files)
                     }
-                    )
+                }
+            )
         }
         /**
          * * We update the text area after the user finishes choosing source and class folders
          */
-        def displayProjectInfo(files: List[java.io.File]) {
+        def displayProjectInfo(files: List[List[java.io.File]]) {
+            val files = loadedFiles(0)
+            val sources = loadedFiles(1)
+            infoText.text() = ""
             if (!files.isEmpty) {
                 if (files(0) != null) {
-                    cp = "-cp="+files(0).toString()
-                            infoText.text = cp+"\n"
-                            // analyseButton.disable = false
+                    //project = Project(files(0))
+                    project = setupProject(files, List[File]())
+                    val iterable = project.statistics.iterator
+                    for (item ← iterable) {
+                        infoText.text() += item.getKey()+" : "+item.getValue().toString+"\n"
+                    }
+
+                    //infoText.text = project
                 }
-                if (files(1) != null) {
+                if (sources(0) != null) {
                     sourceDir = files(1)
-                            infoText.text = infoText.text.value.toString()+"\n Source directory is set to: "+sourceDir.toString()
+                    infoText.text() += "\n Source directory is set to: "+sourceDir.toString()
                 }
 
             }
@@ -150,134 +168,145 @@ object bugPicker extends JFXApp {
 
         val menuBar = new MenuBar {
             menus = List(
-                    new Menu("File") {
-                        items = List(
-                                new MenuItem("Open") {
-                                    onAction = { e: ActionEvent ⇒
-                                            {
-                                        files = loadProjectStage
-                                                if (!files.isEmpty) {
-                                                    displayProjectInfo(files)
-                                                }
-                                            }
+                new Menu("File") {
+                    items = List(
+                        new MenuItem("Open") {
+                            onAction = { e: ActionEvent ⇒
+                                {
+                                    loadedFiles = loadProjectStage
+                                    if (!loadedFiles(0).isEmpty) {
+                                        displayProjectInfo(loadedFiles)
                                     }
                                 }
+                            }
+                        }
 
-                                )
-                    },
-                    analyseButton
                     )
+                },
+                analyseButton
+            )
         }
 
         val downSplitpane = new SplitPane {}
         downSplitpane.getItems().addAll(resultWebview, sourceWebview)
 
         List(menuBar, infoText, downSplitpane)
-}
-
-/**
- * The open File dialog box. Preferrebly this should change into a dialog box
- *  instead of a whole stage and scene
- */
-def loadProjectStage(): List[java.io.File] = {
-        var jars: java.io.File = null
-                var sources: java.io.File = null
-                val outStage = new Stage {
-    outer ⇒
-    title = "Load project files"
-    width = 400
-    height = 200
-    scene = new Scene {
-        root = new BorderPane {
-            //top = 
-            val vbox = new VBox()
-            vbox.content = {
-                //content = 
-                List(
-                        new Label {
-                            text = "Select files(jars/.class/directory) to be analysed"
-                        },
-                        new Button {
-                            id = "Select a Jar/Class File"
-                                    text = "Open"
-                                    onAction = { e: ActionEvent ⇒
-                                    {
-                                val fcb = new FileChooser {
-                                    title = "Open Dialog"
-                                }
-                                fcb.extensionFilters.addAll(
-                                        new FileChooser.ExtensionFilter("Jar Files", "*.jar"),
-                                        new FileChooser.ExtensionFilter("Class Files", "*.class"))
-                                        jars = fcb.showOpenDialog(vbox.getScene().getWindow())
-
-                                    }
-                            }
-                        },
-                        new Label {
-                            text = "Choose the  class directory containing jar of class files"
-                        },
-                        new Button {
-                            text = "Open"
-                                    onAction = { e: ActionEvent ⇒
-                                    {
-                                val dc = new DirectoryChooser {
-                                    title = "Select Directory"
-                                }
-                                jars = dc.showDialog(vbox.getScene().window())
-
-                                    }
-                            }
-                        },
-                        new Label {
-                            text = "select the source directory of your project"
-                        },
-
-                        new Button {
-                            text = "Open"
-                                    onAction = { e: ActionEvent ⇒
-                                    {
-                                val dc = new DirectoryChooser {
-                                    title = "Open Dialog"
-                                }
-
-                                sources = dc.showDialog(vbox.getScene().getWindow())
-
-                                    }
-                            }
-                        },
-                        new Button {
-                            text = "Cancel"
-                                    onAction = { e: ActionEvent ⇒
-                                    {
-                                outer.close
-                                    }
-                            }
-
-                        },
-                        new Button {
-                            text = "Finish"
-                                    onAction = { e: ActionEvent ⇒
-                                    {
-                                println("finished")
-                                outer.close()
-                                    }
-                            }
-
-                        }
-                        )
-
-            }
-            top = vbox
-
-        }
     }
 
-}
-outStage.showAndWait
-//if ( (sources != null) && (jars != null ))
-val results = List[java.io.File](jars, sources)
-results
+    /**
+     * The open File dialog box. Preferrebly this should change into a dialog box
+     *  instead of a whole stage and scene
+     */
+    def loadProjectStage: List[List[File]] = {
+        var jars: List[java.io.File] = List()
+        var sources: java.io.File = null
+        var cancelled = false
+        val listview = new ListView[String](new ObservableBuffer[String]())
+        val outStage = new Stage {
+            outer ⇒
+            title = "Load project files"
+            width = 800
+            height = 400
+            scene = new Scene {
+                root = new BorderPane {
+                    //top = 
+                    val vbox = new VBox()
+                    vbox.content = {
+                        //content = 
+                        List(
+                            new Label {
+                                text = "Select files(jars/.class/directory) to be analysed"
+                            },
+                            new Button {
+                                id = "Select a Jar/Class File"
+                                text = "Add Jar/Class Files"
+                                onAction = { e: ActionEvent ⇒
+                                    {
+                                        val fcb = new FileChooser {
+                                            title = "Open Dialog"
+                                        }
+                                        fcb.extensionFilters.addAll(
+                                            new FileChooser.ExtensionFilter("Jar Files", "*.jar"),
+                                            new FileChooser.ExtensionFilter("Class Files", "*.class"))
+                                        jars :::= List(fcb.showOpenDialog(vbox.getScene().getWindow()))
+                                        listview.items() += jars(0).toString()
 
-}
+                                    }
+                                }
+                            },
+                            new Label {
+                                text = "Choose the  class directory containing jar of class files"
+                            },
+                            new Button {
+                                text = "Open"
+                                onAction = { e: ActionEvent ⇒
+                                    {
+                                        val dc = new DirectoryChooser {
+                                            title = "Select Directory"
+                                        }
+                                        jars :::= List(dc.showDialog(vbox.getScene().window()))
+                                        listview.items() += jars(0).toString()
+
+                                    }
+                                }
+                            },
+                            new Label {
+                                text = "select the source directory of your project"
+                            },
+
+                            new Button {
+                                text = "Open"
+                                onAction = { e: ActionEvent ⇒
+                                    {
+                                        val dc = new DirectoryChooser {
+                                            title = "Open Dialog"
+                                        }
+
+                                        sources = dc.showDialog(vbox.getScene().getWindow())
+
+                                    }
+                                }
+                            },
+
+                            listview,
+
+                            new Button {
+                                text = "Cancel"
+                                onAction = { e: ActionEvent ⇒
+                                    {
+                                        cancelled = true
+                                        outer.close
+                                    }
+                                }
+
+                            },
+                            new Button {
+                                text = "Finish"
+                                onAction = { e: ActionEvent ⇒
+                                    {
+
+                                        outer.close()
+                                    }
+                                }
+
+                            }
+
+                        )
+
+                    }
+                    top = vbox
+
+                }
+            }
+
+        }
+        outStage.showAndWait
+        if (cancelled) {
+            jars = null
+        }
+        val results = List(jars, List(sources))
+        results
+
+    }
 
 }
