1a169ecd6e66bd9ec77f930a71bd4a30831e75c1
Michael Eichberg <mail@michael-eichberg.de>
2014-09-14 07:52:45 +0200

    tracking progress - WIP

--- OPAL/br/src/main/scala/org/opalj/br/CommonSourceElementAttributes.scala
+++ OPAL/br/src/main/scala/org/opalj/br/CommonSourceElementAttributes.scala
@@ -48,7 +48,7 @@ trait CommonSourceElementAttributes extends CommonAttributes {
             case Some(annotations) ⇒ annotations
             case None              ⇒ IndexedSeq.empty
         }
-
+        
     def annotations: Annotations =
         runtimeVisibleAnnotations ++ runtimeInvisibleAnnotations
 
