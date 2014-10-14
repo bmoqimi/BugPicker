b4a5e579b8fec95f21539dc1eae5874e2587fa8c
Arne Lottmann <arne.lottmann@stud.tu-darmstadt.de>
2014-10-14 22:15:15 +0200

    added all directories to project info

--- OPAL/bp/src/main/scala/org/opalj/bugpicker/ProjectInfoDialog.scala
+++ OPAL/bp/src/main/scala/org/opalj/bugpicker/ProjectInfoDialog.scala
@@ -16,19 +16,20 @@ import scalafx.geometry.Pos
 import scalafx.scene.Scene
 
 object ProjectInfoDialog {
+    def toUL(files: Seq[File]): String = files.map(_.getAbsolutePath).mkString("<ul><li>", "</li><li>", "</li></ul>")
+
     def show(owner: Stage, project: Project[URL], sources: Seq[File]) {
+        val (jarDirs, libDirs, sourceDirs) = BugPicker.loadPreferences()
+
+        val html = report(project, jarDirs, libDirs, sourceDirs)
+
         val stage = new DialogStage(owner) {
             theStage ⇒
             scene = new Scene {
                 root = new BorderPane {
                     center = new WebView {
                         contextMenuEnabled = false
-                        engine.loadContent("<html><body style=\"font: 14px sans-serif;\">"+
-                            "<h2>Project statistics</h2>"+
-                            statistics(project)+
-                            "<h2>Source directories</h2>"+
-                            sourceInfo(sources)+
-                            "</body></html>")
+                        engine.loadContent(html)
                     }
                     bottom = new HBox {
                         content = new Button {
@@ -50,9 +51,30 @@ object ProjectInfoDialog {
 
     private def statistics(project: Project[URL]): String =
         if (project == null) ""
-        else project.statistics.toList.map(e ⇒ s"<p>${e._1}: ${e._2}</p>").mkString("\n")
+        else project.statistics.toList.map(e ⇒ s"<li>${e._1}: ${e._2}</li>").mkString("<ul>", "", "</ul>")
 
-    private def sourceInfo(sources: Seq[File]): String =
-        if (sources == null) ""
-        else sources.map(f ⇒ s"<p>${f.getAbsolutePath}</p>").mkString("\n")
+    private def report(project: Project[URL], jarDirs: Seq[File], libDirs: Seq[File], sourceDirs: Seq[File]): String =
+        <html>
+            <head>
+                <style type="text/css"><![CDATA[
+body {
+	font: 14px sans-serif;
+}
+ul, li {
+    list-style-type: none;
+    padding-left: 0;
+}
+    				]]></style>
+            </head>
+            <body>
+                <h2>Project statistics</h2>
+                <ul>{ project.statistics.toList.map(e ⇒ <li>{ e._1 }: { e._2 }</li>) }</ul>
+                <h2>Loaded jar files and directories</h2>
+                <ul>{ jarDirs.map(d ⇒ <li>{ d.getAbsolutePath }</li>) }</ul>
+                <h2>Loaded libraries</h2>
+                <ul>{ libDirs.map(d ⇒ <li>{ d.getAbsolutePath }</li>) }</ul>
+                <h2>Loaded source directories</h2>
+                <ul>{ sourceDirs.map(d ⇒ <li>{ d.getAbsolutePath }</li>) }</ul>
+            </body>
+        </html>.toString
 }
